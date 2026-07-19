8b88bb831374c0760e85c2a55cfe05bc9d3502f8
itd <itd220222@gmail.com>
2026-07-19 12:13:12 +0300

    sdenv.sh: wezterm still revert history related variables

--- sdenv.sh
+++ sdenv.sh
@@ -1,5 +1,5 @@
 alias vim='gvim -v'
 export EDITOR="gvim -v"
 export VISUAL="$EDITOR"
-export HISTIGNORE='?:??:clear:history:pwd'
-export HISTORYCONTROL=ignoreboth
+export HISTIGNORE='?:??:clear:history:pwd:df:git st:ls'
+export HISTCONTROL=ignoreboth
